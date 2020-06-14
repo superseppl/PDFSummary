118b94071ba14f9baa8d03bb1ae8930e4653ccf4
superseppl <se.bader@yahoo.de>
2020-06-14 13:11:36 +0200

    improved comment and vertical line cut out, added rectangle cut out

--- pdfsummary.py
+++ pdfsummary.py
@@ -21,18 +21,35 @@ for i in range(nPages):
     if '/Annots' in page:
         for annot in page['/Annots']:
             tmp_page = copy(page)
-            #print annot.getData()
-            if '/QuadPoints' in annot.getObject():
-                print annot.getObject()['/QuadPoints']
-                tmp_page.cropBox.upperLeft = annot.getObject()['/QuadPoints'][0:2]
-                tmp_page.cropBox.upperRight = annot.getObject()['/QuadPoints'][2:4]
-                tmp_page.cropBox.lowerLeft = annot.getObject()['/QuadPoints'][4:6]
-                tmp_page.cropBox.lowerRight = annot.getObject()['/QuadPoints'][6:8]
+
+            # Cut out each square comment
+            if annot.getObject()['/Subtype'] == '/Square':
+                print annot.getObject()['/Rect']
+                tmp_page.cropBox.upperLeft = annot.getObject()['/Rect'][0:2]
+                tmp_page.cropBox.lowerRight = annot.getObject()['/Rect'][2:4]
+                output.addPage(tmp_page)
+            
+            # Cut out each marked text
+            if annot.getObject()['/Subtype'] == '/Highlight': 
+                print annot.getObject()['/Rect']
+                tmp_page.cropBox.upperLeft = annot.getObject()['/Rect'][0:2]
+                tmp_page.cropBox.lowerRight = annot.getObject()['/Rect'][2:4]
                 output.addPage(tmp_page)
 
-outputStream = file("test", "wb")
+            # Cut out whole page for each vertical line 
+            if annot.getObject()['/Subtype'] == '/Line':  
+                print annot.getObject()['/Rect']
+                tmp_page.cropBox.upperLeft = (0, annot.getObject()['/Rect'][1])
+                tmp_page.cropBox.lowerRight = (page.mediaBox.getUpperRight_x(), annot.getObject()['/Rect'][3])
+                output.addPage(tmp_page)
+
+            
+                
+
+outputStream = file("test.pdf", "wb")
 output.write(outputStream)
 outputStream.close()
 
 #/Rect defines the bounding box of the comment on the page/spread, in points (1/72 of an inch) relative to the lower-left corner of the page, increasing values going right and up.
 # y |_ x
+# For different pdf annotators: if '/QuadPoints' in annot.getObject(): # if '/L' in annot.getObject():
